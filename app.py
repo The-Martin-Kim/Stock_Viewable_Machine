c18922c8971e1595449e63a3c311b2b5c235525c
Martin Kim <127666042+The-Martin-Kim@users.noreply.github.com>
2024-04-05 01:10:57 +0900

    [feat] 주식 종목명 및 날짜 오류 표기

--- app.py
+++ app.py
@@ -15,23 +15,33 @@ with st.sidebar:
         datetime.datetime.now()
     )
 
-    code = st.text_input(
-        '종목 코드',
-        value='',
-        placeholder='종목 코드를 입력해 주세요'
-    )
+    code = st.text_input('종목 코드', value='',
+                         placeholder='종목 코드를 입력해 주세요')
 
 start_date_str = start_date.strftime('%Y%m%d')
 end_date_str = end_date.strftime('%Y%m%d')
 
-if code and start_date and end_date:
-    df = stock.get_market_ohlcv(start_date_str, end_date_str, code)
-    data = df.sort_index(ascending=True).loc[:, '종가']
+if start_date > end_date:
+    st.error('시작일은 종료일보다 이전이어야 합니다. 날짜 범위를 다시 설정해 주세요.')
+else:
+    if code:
+        try:
+            ticker_name = stock.get_market_ticker_name(code)
+            st.header(f'{ticker_name} {code}')
 
-    tab1, tab2 = st.tabs(['Chart', 'Data'])
+            start_date_str = start_date.strftime('%Y%m%d')
+            end_date_str = end_date.strftime('%Y%m%d')
 
-    with tab1:
-        st.line_chart(data)
+            df = stock.get_market_ohlcv(start_date_str, end_date_str, code)
+            data = df.sort_index(ascending=True).loc[:, '종가']
 
-    with tab2:
-        st.dataframe(df.sort_index(ascending=False))
+            tab1, tab2 = st.tabs(['Chart', 'Data'])
+
+            with tab1:
+                st.line_chart(data)
+
+            with tab2:
+                st.dataframe(df.sort_index(ascending=False))
+
+        except ValueError:
+            st.error('유효하지 않은 종목 코드입니다. 정확한 종목 코드를 입력해 주세요.')
